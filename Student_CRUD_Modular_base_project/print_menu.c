28bc7d77a2415e8b61793e70dc0a1e7961a5a3c2
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-07 11:11:04 +0530

    print_menu.c add menu function print

---
+++ Student_CRUD_Modular_base_project/print_menu.c
@@ -0,0 +1,10 @@
+#include "student.h"
+
+void print_menu(){
+    printf("%s\n\n1.Add Student%s", YELLOW, COLOR_END);
+    printf("%s\n2.View All Student%s" , YELLOW, COLOR_END);
+    printf("%s\n3.Search Student%s" , YELLOW, COLOR_END);
+    printf("%s\n4.Update Student%s ", YELLOW, COLOR_END);
+    printf("%s\n5.Delete Student%s" , YELLOW, COLOR_END);
+    printf("%s\n0.Exit%s" , YELLOW, COLOR_END);
+}
