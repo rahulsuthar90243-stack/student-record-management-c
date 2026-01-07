92bbd168b2b564fedbd8a3972a0d4b9509b4c288
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-07 11:25:32 +0530

    add colors.c to resolve extern constant references

---
+++ Student_CRUD_Modular_base_project/colors.c
@@ -0,0 +1,9 @@
+#include "student.h"
+
+const char* COLOR_END = "\033[0m";
+const char* RED ="\033[1;31m";   //.
+const char* GREEN ="\033[1;32m";  //.
+const char* YELLOW ="\033[1;33m";   //.
+const char* BLUE ="\033[1;34m";   //.
+const char* MAGENTA = "\033[0;35m";  //.
+const char* CYAN = "\033[0;36m";  //.
