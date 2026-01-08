188a75859c436c80042b16653fbbe157c75b0dfc
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-08 11:12:47 +0530

    add update_gpa file and logic function

---
+++ Student_CRUD_Modular_base_project/update_gpa.c
@@ -0,0 +1,34 @@
+# include "student.h"
+
+void update_gpa(Student *student, char course_choice[]){
+   FILE *fp = fopen("student.dat", "r+b");
+    if(fp == NULL){
+        printf("%s\nFile Opening Error!%s", RED, COLOR_END);
+        return;
+    }
+
+    int id, found = 0;
+    printf("\nEnter student current ID: ");
+    scanf("%d", &id);
+    getchar();
+
+    while (fread(student, sizeof(Student), 1, fp)){
+        if(student->id == id && strcmp(student->course, course_choice) == 0){
+         
+            printf("\nEnter student new gpa: ");
+            scanf("%f", &student->gpa);
+
+            fseek(fp, -sizeof(Student), SEEK_CUR);
+            fwrite(student, sizeof(Student), 1, fp);
+            found = 1;
+            printf("%s\nGPA updated successfully.%s", GREEN, COLOR_END);
+            break;
+        }
+    }
+
+    if(found == 0){
+        printf("%s\nStudent not found in this course!!!%s", CYAN, COLOR_END);
+    }
+
+    fclose(fp);    
+}
