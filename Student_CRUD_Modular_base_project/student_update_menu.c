911c2f8456ca8ebb484407369f826a62e74f960c
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-08 10:31:07 +0530

    add student_update_menu file and print menu with function

---
+++ Student_CRUD_Modular_base_project/student_update_menu.c
@@ -0,0 +1,10 @@
+#include "student.h"
+
+void update_menu(){
+    printf("%s\n\nPress 1. to update student ID%s", YELLOW, COLOR_END);
+    printf("%s\nPress 2. to update student name%s", YELLOW, COLOR_END);
+    printf("%s\nPress 3. to update student course%s", YELLOW, COLOR_END);
+    printf("%s\nPress 4. to update student semester%s", YELLOW, COLOR_END);
+    printf("%s\nPress 5. to update student GPA%s", YELLOW, COLOR_END);
+    printf("%s\nPress 0. go to back%s", YELLOW, COLOR_END);
+}
