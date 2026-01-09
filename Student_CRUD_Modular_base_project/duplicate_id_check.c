78e0ac2ef956968385f9efdea0f83988ddcf8417
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-09 10:56:25 +0530

    add function definition duplicate_id

---
+++ Student_CRUD_Modular_base_project/duplicate_id_check.c
@@ -0,0 +1,18 @@
+#include "student.h"
+
+int is_id_exists(int id){
+    FILE *fp = fopen("student.dat", "rb");
+    Student temp;
+    if(fp == NULL){
+        printf("%s\nFile Opening Error!%s", RED, COLOR_END);
+        return;
+    }
+
+    while(fread(&temp, sizeof(Student), 1, fp)){
+        if(temp.id == id){
+            return 1;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
