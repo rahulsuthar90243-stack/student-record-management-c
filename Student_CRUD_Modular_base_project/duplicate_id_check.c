a8f11dbaf1972ea247ebbfd8f7106e14ce729a32
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-09 11:08:38 +0530

    duplicate_id_eheck return to modifying return 0;

--- Student_CRUD_Modular_base_project/duplicate_id_check.c
+++ Student_CRUD_Modular_base_project/duplicate_id_check.c
@@ -5,7 +5,7 @@ int is_id_exists(int id){
     Student temp;
     if(fp == NULL){
         printf("%s\nFile Opening Error!%s", RED, COLOR_END);
-        return;
+        return 0;
     }
 
     while(fread(&temp, sizeof(Student), 1, fp)){
