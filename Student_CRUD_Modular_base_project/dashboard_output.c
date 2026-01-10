47ee834c1f003c1edf4e9767f4258c9e1aa651db
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-10 14:44:42 +0530

    add color print statement

--- Student_CRUD_Modular_base_project/dashboard_output.c
+++ Student_CRUD_Modular_base_project/dashboard_output.c
@@ -2,17 +2,17 @@
 
 void dashboard_output(int total_students, float avg_gpa, float highest_gpa, CourseStat stats[], int course_total){
 
-     printf("\n========== STATISTICS DASHBOARD ==========");
+     printf("%s\n========== STATISTICS DASHBOARD ==========%s",  CYAN, COLOR_END);
      printf("\n\n");
      printf("Total_Student   :%d", total_students);
      printf("\nAverage GPA     :%.2f", avg_gpa);
      printf("\nHighest GPA     :%.2f", highest_gpa);
 
-     printf("\n\nCourse-wise Student Count:");
+     printf("%s\n\nCourse-wise Student Count:%s", CYAN, COLOR_END);
       
      for(int i = 0; i < course_total; i++){
         printf("\n%-10s   :%d", stats[i].course, stats[i].count);
      }
 
-    printf("\n=========================================\n");
+    printf("%s\n=========================================\n%s", CYAN, COLOR_END);
 }
