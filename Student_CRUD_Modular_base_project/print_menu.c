ba4e3165b6152e06d404f77630096f1a025ae424
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-11 22:12:23 +0530

    Implement print menu and restore feature

--- Student_CRUD_Modular_base_project/print_menu.c
+++ Student_CRUD_Modular_base_project/print_menu.c
@@ -1,14 +1,14 @@
 #include "student.h"
 
 void print_menu(){
-    printf("%s\n\n1.Add Student%s", YELLOW, COLOR_END);
-    printf("%s\n2.View All Student%s" , YELLOW, COLOR_END);
-    printf("%s\n3.Search Student%s" , YELLOW, COLOR_END);
-    printf("%s\n4.Update Student%s ", YELLOW, COLOR_END);
-    printf("%s\n5.Delete Student%s" , YELLOW, COLOR_END);
-    printf("%s\n6.view statistics%s", YELLOW, COLOR_END);
-    printf("%s\n7.Backup Student Data%s", YELLOW, COLOR_END);
-    printf("%s\n8.Restore Student Data%s", YELLOW, COLOR_END);
-    printf("%s\n9.View Search History\n", YELLOW, COLOR_END);
-    printf("%s\n0.Exit%s" , YELLOW, COLOR_END);
+    printf("%s\n\n[1]  Add New Student%s",YELLOW, COLOR_END);
+    printf("%s\n[2]  View All Students%s", YELLOW, COLOR_END);
+    printf("%s\n[3]  Search Student Record%s", YELLOW, COLOR_END);
+    printf("%s\n[4]  Update Student Record%s", YELLOW, COLOR_END);
+    printf("%s\n[5]  Delete Student Record%s", YELLOW, COLOR_END);
+    printf("%s\n[6]  View Statistical Summary%s", YELLOW, COLOR_END);
+    printf("%s\n[7]  Backup Student Database%s", YELLOW, COLOR_END);
+    printf("%s\n[8]  Restore Student Database%s", YELLOW, COLOR_END);
+    printf("%s\n[9]  View Search History Log%s", YELLOW, COLOR_END);
+    printf("%s\n[0]  Exit Application%s", YELLOW, COLOR_END);
 }
